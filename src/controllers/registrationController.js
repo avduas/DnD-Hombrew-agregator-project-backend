3973a60377eec3fbd46864c9a19a37abac6b3f8f
avduas <tankraketa1995@gmail.com>
2025-04-30 23:04:42 +0300

    Refactor registration logic to include name in user registration and improve error handling

--- src/controllers/registrationController.js
+++ src/controllers/registrationController.js
@@ -2,22 +2,43 @@ const pool = require("../config/db");
 const { generateToken } = require("../middlewares/auth");
 
 async function registration(req, res) {
-  const { email, password } = req.body;
+  const { email, password, name } = req.body;
 
   try {
-    const queryText = `
-        INSERT INTO public.registration (email, password)
-        VALUES ($1, $2)
-        RETURNING *
-        `;
-    const result = await pool.query(queryText, [email, password]);
+    const checkQuery = `
+      SELECT * FROM public.registration
+      WHERE email = $1 OR name = $2
+    `;
+    const checkResult = await pool.query(checkQuery, [email, name]);
 
-    res.status(201).json({
-      message: "User registered successfully",
-      user: result.rows[0],
+    if (checkResult.rows.length > 0) {
+      return res.status(400).json({
+        message: "User with this email or nickname already exists"
+      });
+    }
+
+    const insertQuery = `
+      INSERT INTO public.registration (email, password, name)
+      VALUES ($1, $2, $3)
+      RETURNING *
+    `;
+    const insertResult = await pool.query(insertQuery, [email, password, name]);
+    const user = insertResult.rows[0];
+
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      user: user.name,
     });
+
+    res.status(200).json({
+      message: "Registration successful",
+      token: token,
+      user: user.name,
+    });
+
   } catch (error) {
-    console.error("Error saving new user:", error);
+    console.error("Error during registration:", error);
     res.status(500).send("Internal server error");
   }
 }
@@ -37,11 +58,16 @@ async function login(req, res) {
       return res.status(401).json({ message: "Invalid email or password" });
     }
 
-    const token = await generateToken({ id : user.id, email: user.email})
+    const token = await generateToken({
+      id: user.id,
+      email: user.email,
+      user: user.name,
+    });
 
     res.status(200).json({
       message: "Login successful",
       token: token,
+      user: user.name,
     });
   } catch (error) {
     console.error("Login error:", error);
