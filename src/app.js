4bb0d26f1d04fde68da88eacd262a1e402b89269
avduas <tankraketa1995@gmail.com>
2025-05-02 18:43:42 +0300

    .env for port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('./config/db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 const authorization = require("./routes/authorization");
